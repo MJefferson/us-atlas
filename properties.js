5bb390d656f11816d3cbb23b29db01a20c275377
Martial James Jefferson <martial.jefferson@gmail.com>
2022-04-26 18:41:28 -0400

    add additional geographic levels

--- properties.js
+++ properties.js
@@ -2,8 +2,10 @@ const shapefile = require("shapefile");
 
 Promise.all([
   parseInput(),
-  shapefile.read("build/cb_2017_us_county_5m.shp"),
-  shapefile.read("build/cb_2017_us_state_5m.shp")
+  shapefile.read("build/cb_2021_us_cbsa_5m.shp"),
+  shapefile.read("build/cb_2021_us_cd116_5m.shp"),
+  shapefile.read("build/cb_2021_us_county_5m.shp"),
+  shapefile.read("build/cb_2021_us_state_5m.shp")
 ]).then(output);
 
 function parseInput() {
@@ -19,7 +21,9 @@ function parseInput() {
   });
 }
 
-function output([topology, counties, states]) {
+function output([topology, cbsa, cd, counties, states]) {
+  cbsa = new Map(cbsa.features.map(d => [d.properties.GEOID, d.properties]));
+  cd = new Map(cd.features.map(d => [d.properties.GEOID, d.properties]));
   counties = new Map(counties.features.map(d => [d.properties.GEOID, d.properties]));
   states = new Map(states.features.map(d => [d.properties.GEOID, d.properties]));
   for (const county of topology.objects.counties.geometries) {
